6a3bae56b0dfcbd1edaee985a876bd8c580f8def
Evan Laurie <evanlaurie@me.com>
2016-03-20 17:04:03 -0700

    updated copyright date

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
 				<h2>Thanks for Visiting our Website</h2>
 				<div class="footer_txt">				
 					<h3>Mr. &amp; Future Mrs. Laurie</h3>
-					<div class="copyrights">Copyright &copy; 2015 <a href="mailto:evan@thelauries.com">Evan</a> &amp; <a href="mailto:elise@thelauries.com">Elise Laurie</a></div>
+					<div class="copyrights">Copyright &copy; 2016 <a href="mailto:evan@thelauries.com">Evan</a> &amp; <a href="mailto:elise@thelauries.com">Elise Laurie</a></div>
 					<div>Developed by <a target="_blank" href="http://evanlaurie.com">Evan Laurie</a></div>
 				</div>
       </div>
