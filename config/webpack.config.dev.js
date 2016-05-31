31d979d82a57f2eca548c4d3bf3573b934091cf2
Evan Laurie <evanlaurie@me.com>
2016-05-30 22:09:39 -0700

    split webpack config files

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,23 @@
+import webpack from 'webpack';
+import WebpackNotifierPlugin from 'webpack-notifier';
+import {
+  Config,
+  ConfigEnvironment
+} from 'webpack-config';
+ 
+export default new Config().extend('./config/webpack.config.base.js').merge({
+  filename: __filename,
+  debug: true,
+  devtool: 'inline-source-map',
+  output: {
+    pathinfo: true
+  },
+  plugins: [
+    new WebpackNotifierPlugin({alwaysNotify: true}),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ]
+});
